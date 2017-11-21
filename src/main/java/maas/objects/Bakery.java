0d400ad83db705ddd137aeed7dc9bf2e001eb12b
Tim Metzler <tim.metzler@inf.h-brs.de>
2017-11-21 17:06:35 +0100

    Created a bakery object that stores the orders and products and general information about the bakery

---
+++ src/main/java/maas/objects/Bakery.java
@@ -0,0 +1,80 @@
+package maas.objects;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import maas.utils.OrderDueDateComparator;
+
+public class Bakery {
+
+	private String guiId;
+	private String name;
+	private int locationX;
+	private int locationY;
+
+	private List<Order> orders;
+	private List<Order> ordersInProcess;
+	private Map<String, Product> cookBook;
+
+	public Bakery(String guiId, String name, int locationX, int locationY) {
+		this.guiId = guiId;
+		this.name = name;
+		this.locationX = locationX;
+		this.locationY = locationY;
+
+		this.orders = new LinkedList<Order>();
+		this.ordersInProcess = new LinkedList<Order>();
+		this.cookBook = new HashMap<String, Product>();
+
+		System.out.println("Bakery " + name + " at location (" + locationX + ", " + locationY + ")");
+	}
+
+	public Product getProductByName(String productName) {
+		if (this.cookBook.containsKey(productName)) {
+			return this.cookBook.get(productName);
+		}
+		return null;
+	}
+	
+	public int getPrice(Order order) {
+		int price = 0;
+		
+		String[] productIds = order.getProductIds();
+		int[] productAmount = order.getProductAmounts();
+		
+		for (int i=0;i<productIds.length;i++) {
+			Product product = getProductByName(productIds[i]);
+			if (product == null) {
+				return -1;
+			}
+			price += productAmount[i]*product.getSalesPrice();
+		}
+		
+		return price;
+	}
+
+	public void addOrder(Order order) {
+		this.orders.add(order);
+		Collections.sort(this.orders, new OrderDueDateComparator());
+	}
+
+	public void addProduct(Product product) {
+		String productId = product.getId();
+		this.cookBook.put(productId, product);
+		//System.out.println("Added product to cookbook:\n" + product.toString());
+	}
+
+	public Order getOrder() {
+		if (this.orders.size() > 0) {
+			Order order = this.orders.get(0);
+			this.ordersInProcess.add(order);
+			this.orders.remove(0);
+			return order;
+		}
+		return null;
+	}
+
+}
