fa1dc8d16f0d4d0e5a4611b53b5780c45a88366a
Tim Metzler <tim.metzler@inf.h-brs.de>
2017-11-22 18:25:10 +0100

    Order agent now uses yellow pages to advertise a service and gives proper replies

--- src/main/java/maas/agents/OrderAgent.java
+++ src/main/java/maas/agents/OrderAgent.java
@@ -5,6 +5,10 @@ import java.util.List;
 
 import jade.core.Agent;
 import jade.core.behaviours.*;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
 import jade.lang.acl.ACLMessage;
 import jade.util.Logger;
 import maas.objects.Bakery;
@@ -15,6 +19,7 @@ public class OrderAgent extends Agent {
 	
 	private List<Order> orders = new LinkedList<Order>();
 	private Bakery myBakery;
+	private Logger logger;
 	
 	public OrderAgent(Bakery bakery) {
 		this.myBakery = bakery;
@@ -22,6 +27,8 @@ public class OrderAgent extends Agent {
 	
 	@Override
 	protected void setup() {
+		// Create our logger
+		logger = Logger.getJADELogger(this.getClass().getName());
 		// Printout a welcome message
 		System.out.println("Hello! Baker-agent " + getAID().getName() + " is ready.");
 
@@ -32,15 +39,33 @@ public class OrderAgent extends Agent {
 			logger.log(Logger.WARNING, e.getMessage(), e);	
 			Thread.currentThread().interrupt();
 		}
+		// Register the bakery service in the yellow pages
+		DFAgentDescription dfd = new DFAgentDescription();
+		dfd.setName(getAID());
+		ServiceDescription sd = new ServiceDescription();
+		sd.setType("bakery");
+		sd.setName("Bakery-ordering");
+		dfd.addServices(sd);
+		try {
+			DFService.register(this, dfd);
+		} catch (FIPAException fe) {
+			logger.log(Logger.WARNING, fe.getMessage(), fe);	
+		}
 		addBehaviour(new OrderService());
 	}
 
 	@Override
 	protected void takeDown() {
+		// Remove from the yellow pages
+		try {
+			DFService.deregister(this);
+		} catch (FIPAException fe) {
+			logger.log(Logger.WARNING, fe.getMessage(), fe);
+		}
 		System.out.println(getAID().getLocalName() + ": Terminating.");
 	}
 
-	// cyclic order receiving behavior
+	// Cyclic order receiving behavior
 	private class OrderService extends CyclicBehaviour {
 		public void action() {
 
@@ -50,12 +75,19 @@ public class OrderAgent extends Agent {
 				Order order = new Order(jsonOrder);
 				
 				// Get the price of the order
-				int price = myBakery.getPrice(order);
-				orders.add(order);
-				System.out.println("So you want " + order);
+				Integer price = myBakery.getPrice(order);
 				ACLMessage reply = msg.createReply();
-				reply.setPerformative(ACLMessage.PROPOSE);
-				reply.setContent("Give me EUR " + price);
+				if (price != null) {
+					reply.setPerformative(ACLMessage.PROPOSE);
+					reply.setContent(String.valueOf(price));
+				} else {
+					reply.setPerformative(ACLMessage.REFUSE);
+					reply.setContent("not-available");
+				}
+				//orders.add(order);
+				//System.out.println("So you want " + order);
+				
+
 				myAgent.send(reply);
 			} else {
 				block();
