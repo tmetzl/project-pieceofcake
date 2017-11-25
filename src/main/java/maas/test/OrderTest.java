6b034326f31b4dec5385f930f4c7b07f2aba63ac
Tim Metzler <tim.metzler@inf.h-brs.de>
2017-11-25 23:20:12 +0100

    Changed test values

--- src/main/java/maas/test/OrderTest.java
+++ src/main/java/maas/test/OrderTest.java
@@ -12,7 +12,7 @@ public class OrderTest {
 	@Test
 	public void jsonParseToOrder() {
 		String jsonOrder = "{\"order_date\": {\"day\": 1,\"hour\": 6},"
-				+ "\"guid\": \"order-001\",\"products\": {\"Brezel\": 6,\"Bread\": 16},"
+				+ "\"guid\": \"order-001\",\"products\": {\"Brezel\": 7,\"Bread\": 16},"
 				+ "\"customer_id\": \"customer-001\"," + "\"delivery_date\": {\"day\": 2,\"hour\": 19}}";
 		Order order = new Order(jsonOrder);
 		
@@ -26,7 +26,7 @@ public class OrderTest {
 		assertTrue("Bread".equals(productIds[1]));
 		int[] productAmounts = order.getProductAmounts();
 		assertEquals(2, productAmounts.length);
-		assertEquals(6, productAmounts[0]);
+		assertEquals(7, productAmounts[0]);
 		assertEquals(16, productAmounts[1]);
 
 	}
