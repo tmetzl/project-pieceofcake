da1c4551c8efbdaacc80f78844dd1ef229306b72
Tim Metzler <tim.metzler@inf.h-brs.de>
2017-11-26 17:43:36 +0100

    Changed use of Scanner to try-with-resources

--- src/main/java/maas/main/Start.java
+++ src/main/java/maas/main/Start.java
@@ -42,30 +42,27 @@ public class Start {
 		Profile profile = new ProfileImpl(properties);
 
 		container = runtime.createMainContainer(profile);
-		
 
 		loadScenario(scenario);
 	}
 
 	public void loadScenario(String filename) throws StaleProxyException {
 
-		Scanner in = null;
-		try {
-			in = new Scanner(new FileReader("config/" + filename + ".json"));
-			
+		try (Scanner in = new Scanner(new FileReader("config/" + filename + ".json"))) {
+
 			StringBuilder bld = new StringBuilder();
 			while (in.hasNext())
-			    bld.append(in.nextLine());
-			
+				bld.append(in.nextLine());
+
 			String text = bld.toString();
 
 			// Parse scenario as JSONObject
 			JSONObject scenario = new JSONObject(text);
-			
+
 			// Step 1: Process bakeries
 			JSONArray bakeries = scenario.getJSONArray("bakeries");
-			
-			for (int i=0;i<bakeries.length();i++) {
+
+			for (int i = 0; i < bakeries.length(); i++) {
 				JSONObject bakery = bakeries.getJSONObject(i);
 				createBakery(bakery);
 			}
@@ -113,14 +110,9 @@ public class Start {
 					createCustomer(customer, customerOrders);
 				}
 			}
-			
-
 
 		} catch (FileNotFoundException e) {
-			logger.log(Logger.WARNING, e.getMessage(), e);	
-		} finally {
-			if (in != null)
-				in.close();
+			logger.log(Logger.WARNING, e.getMessage(), e);
 		}
 
 	}
@@ -139,23 +131,23 @@ public class Start {
 		container.acceptNewAgent(name, agent).start();
 
 	}
-	
+
 	public void createBakery(JSONObject jsonBakery) throws StaleProxyException {
 		String name = jsonBakery.getString("name");
 		String guiId = jsonBakery.getString("guid");
 		JSONObject location = jsonBakery.getJSONObject("location");
 		int locationX = location.getInt("x");
 		int locationY = location.getInt("y");
-		
+
 		Bakery bakery = new Bakery(guiId, name, locationX, locationY);
-		
+
 		JSONArray products = jsonBakery.getJSONArray("products");
-		for (int i=0;i<products.length();i++) {
+		for (int i = 0; i < products.length(); i++) {
 			JSONObject jsonProduct = products.getJSONObject(i);
 			Product product = new Product(jsonProduct.toString());
 			bakery.addProduct(product);
 		}
-		
+
 		container.acceptNewAgent(name, new OrderAgent(bakery)).start();
 	}
 
