ce540b7aee07cb536602c9ee3e94409cc00bb0ce
sogolhaghighat <sogol.haghighat@gmail.com>
2017-11-27 17:43:53 +0100

    Created the basic kneading behaviours for KneadingAgent

---
+++ src/main/java/maas/agents/KneadingAgent.java
@@ -0,0 +1,114 @@
+package maas.agents;
+
+import jade.core.AID;
+import jade.core.Agent;
+import jade.core.behaviours.Behaviour;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.core.behaviours.OneShotBehaviour;
+import jade.core.behaviours.SequentialBehaviour;
+import jade.lang.acl.ACLMessage;
+
+public class KneadingAgent extends Agent {
+
+	@Override
+	protected void setup() {
+		// TODO: use the behaviour
+
+	}
+
+	@Override
+	protected void takeDown() {
+		System.out.println(getAID().getLocalName() + ": Terminating.");
+	}
+
+	private class ProcessKneadingRequest extends SequentialBehaviour {
+
+		private AID kneadingScheduler;
+		private String request;
+		private long kneadingTime;
+
+		public ProcessKneadingRequest() {
+			this.addSubBehaviour(new ReceiveKneadingRequest());
+			this.addSubBehaviour(new Knead(kneadingTime));
+			this.addSubBehaviour(new RespondToKneadingRequest());
+
+		}
+
+		private class ReceiveKneadingRequest extends Behaviour {
+
+			private boolean requestReceived = false;
+
+			@Override
+			public void action() {
+				ACLMessage msg = myAgent.receive();
+				if (msg != null && msg.getPerformative() == ACLMessage.REQUEST) {
+					request = msg.getContent();					
+					kneadingScheduler = msg.getSender();
+					// TODO: extract kneading time
+					requestReceived = true;
+
+				} else {
+					block();
+				}
+
+			}
+
+			@Override
+			public boolean done() {
+				return requestReceived;
+			}
+
+		}
+
+		private class Knead extends Behaviour {
+
+			private boolean kneadingFinished = false;
+			private long kneadingTime;
+			private long startingTime;
+
+			public Knead(long kneadingTime) {
+				this.kneadingTime = kneadingTime;
+			}
+
+			@Override
+			public void onStart() {
+				this.startingTime = System.currentTimeMillis();
+
+			}
+
+			@Override
+			public void action() {
+				long currentTime = System.currentTimeMillis();
+				long remainingTime = kneadingTime - (currentTime - startingTime);
+				if (remainingTime >= 0) {
+					kneadingFinished = true;
+				} else {
+					block(remainingTime);
+				}
+
+			}
+
+			@Override
+			public boolean done() {
+				return kneadingFinished;
+			}
+
+		}
+		
+		private class RespondToKneadingRequest extends OneShotBehaviour {
+
+			@Override
+			public void action() {
+				ACLMessage reply = new ACLMessage(ACLMessage.INFORM);
+				reply.addReceiver(kneadingScheduler);
+				reply.setContent(request);
+				reply.setLanguage("English");
+				reply.setOntology("Bakery-order-ontology");
+				myAgent.send(reply);
+				
+			}
+			
+		}
+
+	}
+}
