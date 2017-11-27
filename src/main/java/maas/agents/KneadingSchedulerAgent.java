acfe828438ec8b472b9dbd08db2b89b5e6e08e90
sogolhaghighat <sogol.haghighat@gmail.com>
2017-11-27 21:54:01 +0100

    Added a welcome message

--- src/main/java/maas/agents/KneadingSchedulerAgent.java
+++ src/main/java/maas/agents/KneadingSchedulerAgent.java
@@ -5,19 +5,18 @@ import jade.core.Agent;
 
 @SuppressWarnings("serial")
 public class KneadingSchedulerAgent extends Agent {
-	
+
 	private AID[] kneadingAgents;
-	
-	
+
 	@Override
 	protected void setup() {
-		
+		// Printout a welcome message
+		System.out.println("Hello! Kneading-Scheduler " + getAID().getName() + " is ready.");
 	}
-	
+
 	@Override
 	protected void takeDown() {
 		System.out.println(getAID().getLocalName() + ": Terminating.");
 	}
 
 }
-
