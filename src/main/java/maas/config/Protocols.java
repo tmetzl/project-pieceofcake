5aecd06d525b81dba6107ec5e70a8ad68f4bcdfb
Tim Metzler <tim.metzler@inf.h-brs.de>
2017-11-29 00:42:40 +0100

    Added a file for protocol constants

---
+++ src/main/java/maas/config/Protocols.java
@@ -0,0 +1,9 @@
+package maas.config;
+
+public class Protocols {
+	
+	public static final String STARTUP = "startUp-protocol";
+	public static final String ORDER = "order-protocol";
+	public static final String TIME = "time-protocol";
+
+}
