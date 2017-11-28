fd900f12c879d0cfd62350898b9fc8dc597d232e
Tim Metzler <tim.metzler@inf.h-brs.de>
2017-11-29 00:46:33 +0100

    KneadingScheduler now waits for startup

--- src/main/java/maas/agents/KneadingSchedulerAgent.java
+++ src/main/java/maas/agents/KneadingSchedulerAgent.java
@@ -1,17 +1,25 @@
 package maas.agents;
 
 import jade.core.AID;
-import jade.core.Agent;
+import jade.core.behaviours.SequentialBehaviour;
 
 @SuppressWarnings("serial")
-public class KneadingSchedulerAgent extends Agent {
+public class KneadingSchedulerAgent extends SynchronizedAgent {
 
 	private AID[] kneadingAgents;
 
 	@Override
 	protected void setup() {
+		super.setup();
 		// Printout a welcome message
 		System.out.println("Hello! Kneading-Scheduler " + getAID().getName() + " is ready.");
+		
+		SequentialBehaviour seq = new SequentialBehaviour();
+		
+		seq.addSubBehaviour(new SynchronizeClock());
+		seq.addSubBehaviour(new WaitForStart());
+
+		addBehaviour(seq);
 	}
 
 	@Override
