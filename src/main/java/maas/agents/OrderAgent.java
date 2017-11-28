9f3197f4a9f90a18589c3894f3ed530aec85c613
Tim Metzler <tim.metzler@inf.h-brs.de>
2017-11-29 00:44:30 +0100

    OrderAgent now extends SynchronizedAgent

--- src/main/java/maas/agents/OrderAgent.java
+++ src/main/java/maas/agents/OrderAgent.java
@@ -3,23 +3,23 @@ package maas.agents;
 import java.util.LinkedList;
 import java.util.List;
 
-import jade.core.Agent;
-import jade.core.behaviours.*;
+import jade.core.behaviours.CyclicBehaviour;
 import jade.domain.DFService;
 import jade.domain.FIPAException;
-import jade.domain.FIPAAgentManagement.DFAgentDescription;
 import jade.domain.FIPAAgentManagement.ServiceDescription;
 import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
 import jade.util.Logger;
+import maas.config.Protocols;
 import maas.objects.Bakery;
 import maas.objects.Order;
 
 @SuppressWarnings("serial")
-public class OrderAgent extends Agent {
+public class OrderAgent extends SynchronizedAgent {
 
 	private transient List<Order> orders = new LinkedList<>();
 	private transient Bakery myBakery;
-	private Logger logger;
+	//private Logger logger;
 
 	public OrderAgent(Bakery bakery) {
 		this.myBakery = bakery;
@@ -27,29 +27,25 @@ public class OrderAgent extends Agent {
 
 	@Override
 	protected void setup() {
-		// Create our logger
-		logger = Logger.getJADELogger(this.getClass().getName());
+		super.setup();
 		// Printout a welcome message
 		System.out.println("Hello! Baker-agent " + getAID().getName() + " is ready.");
 
 		try {
-			Thread.sleep(3000);
+			Thread.sleep(1000);
 		} catch (InterruptedException e) {
 			logger.log(Logger.WARNING, e.getMessage(), e);
 			Thread.currentThread().interrupt();
 		}
+		
 		// Register the bakery service in the yellow pages
-		DFAgentDescription dfd = new DFAgentDescription();
-		dfd.setName(getAID());
 		ServiceDescription sd = new ServiceDescription();
 		sd.setType("bakery");
-		sd.setName("Bakery-ordering");
-		dfd.addServices(sd);
-		try {
-			DFService.register(this, dfd);
-		} catch (FIPAException fe) {
-			logger.log(Logger.WARNING, fe.getMessage(), fe);
-		}
+		sd.setName("Bakery-ordering");		
+		registerService(sd);
+		
+		addBehaviour(new SynchronizeClock());
+		addBehaviour(new WaitForStart());
 		addBehaviour(new OrderService());
 	}
 
@@ -67,10 +63,9 @@ public class OrderAgent extends Agent {
 	// Cyclic order receiving behavior
 	private class OrderService extends CyclicBehaviour {
 		public void action() {
-
-			ACLMessage msg = myAgent.receive();
+			MessageTemplate msgTemplate = MessageTemplate.MatchProtocol(Protocols.ORDER);
+			ACLMessage msg = myAgent.receive(msgTemplate);
 			if (msg != null) {
-
 				if (msg.getPerformative() == ACLMessage.CFP) {
 					// Customer wants an offer
 
@@ -96,7 +91,7 @@ public class OrderAgent extends Agent {
 					Order order = new Order(jsonOrder);
 					// Add to active orders
 					orders.add(order);
-					System.out.println(myAgent.getLocalName() + ": Customer " + msg.getSender().getLocalName() + " ordered:\n" + order + ".");
+					//System.out.println(myAgent.getLocalName() + ": Customer " + msg.getSender().getLocalName() + " ordered:\n" + order + ".");
 				}
 			} else {
 				block();
