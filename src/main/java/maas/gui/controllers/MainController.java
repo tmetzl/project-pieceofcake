4dd869b406b503f7d4da3ececd735cc8b964eca9
Tim Metzler <tim.metzler@inf.h-brs.de>
2017-12-17 17:17:49 +0100

    MainController passes street network to MapTabController

--- src/main/java/maas/gui/controllers/MainController.java
+++ src/main/java/maas/gui/controllers/MainController.java
@@ -2,6 +2,7 @@ package maas.gui.controllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.TabPane;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 import maas.main.Scenario;
 
@@ -12,11 +13,16 @@ public class MainController {
 	@FXML
 	private HBox customerTab;
 	@FXML
+	private AnchorPane mapTab;
+	@FXML
 	private CustomerTabController customerTabController;
+	@FXML
+	private MapTabController mapTabController;
 	
 	@FXML
 	private void initialize() {
 		customerTabController.setCustomers(Scenario.getInstance().getCustomers());
+		mapTabController.setStreetNetwork(Scenario.getInstance().getStreetNetwork());
 	}
 
 }
