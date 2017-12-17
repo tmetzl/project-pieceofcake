8ca545c7f4fdf9236b33d2313f413487e6689651
Tim Metzler <tim.metzler@inf.h-brs.de>
2017-12-17 14:27:04 +0100

    GUI Starter now uses the new Scenario class and controllers

--- src/main/java/maas/gui/main/StartGUI.java
+++ src/main/java/maas/gui/main/StartGUI.java
@@ -1,20 +1,38 @@
 package maas.gui.main;
 
-import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
+import java.io.IOException;
 
-import maas.main.Start;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import maas.main.Scenario;
 
 public class StartGUI extends Application {	
 	
 	@Override
 	public void start(Stage stage) {
-		Start start;
-		start = new Start("random-scenario");
-		GUIController controller = new GUIController(start.getCustomers());
-		stage.setScene(new Scene(controller,600,400));
-		stage.show();
+		//Start start;
+		//start = new Start("random-scenario");
+		//MainController controller = new MainController(start.getCustomers());
+
+		try {
+			Scenario scenario = Scenario.getInstance();
+			scenario.load("random-scenario");
+			scenario.start();
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(getClass().getResource("/maas/gui/fxml/main.fxml"));
+			Parent root;
+			root = loader.load();
+			Scene scene = new Scene(root);
+			stage.setScene(scene);
+			stage.show();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 
 	public static void main(String[] args) {
