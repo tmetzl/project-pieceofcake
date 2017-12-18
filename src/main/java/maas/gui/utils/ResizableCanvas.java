e528f926ba0ada7767ea8b2f3785e7189acb0d31
Tim Metzler <tim.metzler@inf.h-brs.de>
2017-12-18 02:01:01 +0100

    Made map resizable

---
+++ src/main/java/maas/gui/utils/ResizableCanvas.java
@@ -0,0 +1,47 @@
+package maas.gui.utils;
+
+import javafx.scene.canvas.Canvas;
+
+public class ResizableCanvas extends Canvas {
+	
+	public void draw() {
+		System.out.println("draw");
+		System.out.println(getWidth()+"\t"+getHeight());
+	}
+	
+	@Override
+	public boolean isResizable() {
+		return true;
+	}
+	
+	@Override
+	public double minWidth(double height) {
+		return 20;
+	}
+	
+	@Override
+	public double maxWidth(double height) {
+		return Double.MAX_VALUE;
+	}
+	
+	@Override
+	public double prefWidth(double height) {
+		return 900;
+	}
+	
+	@Override
+	public double minHeight(double width) {
+		return 20;
+	}
+	
+	@Override
+	public double maxHeight(double width) {
+		return Double.MAX_VALUE;
+	}
+	
+	@Override
+	public double prefHeight(double width) {
+		return 900;
+	}	
+
+}
