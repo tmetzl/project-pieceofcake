e35b505d658bcd6f9620bedb8e6bd4a209fdcc46
Tim Metzler <tim.metzler@inf.h-brs.de>
2017-12-29 17:53:09 +0100

    Added an interface that declares the basic functionality of a schedule

---
+++ src/main/java/maas/interfaces/Schedule.java
@@ -0,0 +1,16 @@
+package maas.interfaces;
+
+import maas.tasks.ScheduledTask;
+import maas.tasks.Task;
+
+public interface Schedule<T extends Task>{
+	
+	public long getEarliestCompletionTime(T task);
+	
+	public void insert(T task);
+	
+	public ScheduledTask<T> getNextScheduledTask();
+	
+	public void removeTask(T task);
+
+}
