1c558297549ad69c8f18ccfe63b60762979c688a
sogolhaghighat <sogol.haghighat@gmail.com>
2018-01-02 23:15:58 +0100

    Adjusted the KneadingTaskTest

--- src/test/java/maas/tasks/KneadingTaskTest.java
+++ src/test/java/maas/tasks/KneadingTaskTest.java
@@ -5,18 +5,21 @@ import static org.junit.Assert.assertEquals;
 import org.json.JSONObject;
 import org.junit.Test;
 
+import maas.objects.Date;
+
 public class KneadingTaskTest {
 
 	@Test
 	public void testGettersAndSetters() {
+		Date releaseDate = new Date(0, 3, 1, 0);
+		Date dueDate = new Date(1, 2, 3, 4);
 		JSONObject jsonObject = new JSONObject();
 		jsonObject.put("product_id", "Bread");
 		jsonObject.put("kneading_time", 10);
 		jsonObject.put("resting_time", 2);
-		jsonObject.put("release_date", 2);
-		jsonObject.put("due_date", 14);
+		jsonObject.put("release_date", releaseDate.toJSONObject());
+		jsonObject.put("due_date", dueDate.toJSONObject());
 		jsonObject.put("order_id", "order-001");
-		jsonObject.put("day", 1);
 
 		KneadingTask kneadingTask = new KneadingTask();
 		kneadingTask.fromJSONObject(jsonObject);
@@ -24,31 +27,19 @@ public class KneadingTaskTest {
 		assertEquals("Bread", kneadingTask.getProductId());
 		assertEquals(10, kneadingTask.getKneadingTime());
 		assertEquals(2, kneadingTask.getRestingTime());
-		assertEquals(2, kneadingTask.getReleaseDate());
-		assertEquals(14, kneadingTask.getDueDate());
+		assertEquals(releaseDate, kneadingTask.getReleaseDate());
+		assertEquals(dueDate, kneadingTask.getDueDate());
 		assertEquals("order-001", kneadingTask.getOrderId());
-		assertEquals(1, kneadingTask.getDay());
 
 		JSONObject jsonObjectFromKneadingTask = kneadingTask.toJSONObject();
-
-		assertEquals(7, jsonObjectFromKneadingTask.length());
-		assertEquals("Bread", jsonObjectFromKneadingTask.getString("product_id"));
-		assertEquals(10l, jsonObjectFromKneadingTask.getLong("kneading_time"));
-		assertEquals(2l, jsonObjectFromKneadingTask.getLong("resting_time"));
-		assertEquals(2l, jsonObjectFromKneadingTask.getLong("release_date"));
-		assertEquals(14l, jsonObjectFromKneadingTask.getLong("due_date"));
-		assertEquals("order-001", jsonObjectFromKneadingTask.getString("order_id"));
-		assertEquals(1, jsonObjectFromKneadingTask.getInt("day"));
-		
-		KneadingTask anotherKneadingTask = new KneadingTask(1, 10, 2, 14, 2, "order-001", "Bread");
+		KneadingTask anotherKneadingTask = new KneadingTask();
+		anotherKneadingTask.fromJSONObject(jsonObjectFromKneadingTask);
 
 		assertEquals("Bread", anotherKneadingTask.getProductId());
 		assertEquals(10, anotherKneadingTask.getKneadingTime());
 		assertEquals(2, anotherKneadingTask.getRestingTime());
-		assertEquals(2, anotherKneadingTask.getReleaseDate());
-		assertEquals(14, anotherKneadingTask.getDueDate());
+		assertEquals(releaseDate, anotherKneadingTask.getReleaseDate());
+		assertEquals(dueDate, anotherKneadingTask.getDueDate());
 		assertEquals("order-001", anotherKneadingTask.getOrderId());
-		assertEquals(1, anotherKneadingTask.getDay());
 	}
-
 }
