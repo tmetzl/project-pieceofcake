4b0f23e8d3357ecd4a77fe3385c38f9bf5de17cc
sogolhaghighat <sogol.haghighat@gmail.com>
2018-01-03 14:44:08 +0100

    Removed code that is now in the Task class

--- src/main/java/maas/tasks/BakingTask.java
+++ src/main/java/maas/tasks/BakingTask.java
@@ -9,65 +9,56 @@ public class BakingTask extends Task {
 	private int numOfItems;
 	private double coolingTimeFactor;
 
-	public BakingTask() {
-
+	public long getBakingTime() {
+		return bakingTime;
 	}
 
-	public BakingTask(int day, long bakingTime, long bakingTemperature, int numOfItems, double coolingTimeFactor,
-			long dueDate, long releaseDate, String orderId, String productId) {
-		this.bakingTemperature = bakingTemperature;
+	public void setBakingTime(long bakingTime) {
 		this.bakingTime = bakingTime;
-		this.numOfItems = numOfItems;
-		this.coolingTimeFactor = coolingTimeFactor;
-		setProductId(productId);
-		setDueDate(dueDate);
-		setReleaseDate(releaseDate);
-		setOrderId(orderId);
-		setDay(day);
-	}
-
-	public int getNumOfItems() {
-		return numOfItems;
 	}
 
 	public long getBakingTemperature() {
 		return bakingTemperature;
 	}
 
-	public long getBakingTime() {
-		return bakingTime;
+	public void setBakingTemperature(long bakingTemperature) {
+		this.bakingTemperature = bakingTemperature;
+	}
+
+	public int getNumOfItems() {
+		return numOfItems;
+	}
+
+	public void setNumOfItems(int numOfItems) {
+		this.numOfItems = numOfItems;
 	}
 
 	public double getCoolingTimeFactor() {
 		return coolingTimeFactor;
 	}
 
+	public void setCoolingTimeFactor(double coolingTimeFactor) {
+		this.coolingTimeFactor = coolingTimeFactor;
+	}
+
 	@Override
 	public JSONObject toJSONObject() {
-		JSONObject jsonObject = new JSONObject();
-		jsonObject.put("product_id", getProductId());
-		jsonObject.put("order_id", getOrderId());
-		jsonObject.put("due_date", getDueDate());
-		jsonObject.put("release_date", getReleaseDate());
+		JSONObject jsonObject = super.toJSONObject();
 		jsonObject.put("baking_time", bakingTime);
 		jsonObject.put("baking_temp", bakingTemperature);
 		jsonObject.put("num_of_items", numOfItems);
 		jsonObject.put("cooling_time_factor", coolingTimeFactor);
-		jsonObject.put("day", getDay());
 		return jsonObject;
 	}
 
 	@Override
 	public void fromJSONObject(JSONObject jsonObject) {
-		setProductId(jsonObject.getString("product_id"));
-		setOrderId(jsonObject.getString("order_id"));
-		setReleaseDate(jsonObject.getLong("release_date"));
-		setDueDate(jsonObject.getLong("due_date"));
-		bakingTime = jsonObject.getLong("baking_time");
-		bakingTemperature = jsonObject.getLong("baking_temp");
-		numOfItems = jsonObject.getInt("num_of_items");
-		coolingTimeFactor = jsonObject.getDouble("cooling_time_factor");
-		setDay(jsonObject.getInt("day"));
+		super.fromJSONObject(jsonObject);
+		setBakingTime(jsonObject.getLong("baking_time"));
+		setBakingTemperature(jsonObject.getLong("baking_temp"));
+		setNumOfItems(jsonObject.getInt("num_of_items"));
+		setCoolingTimeFactor(jsonObject.getDouble("cooling_time_factor"));
+
 	}
 
 }
