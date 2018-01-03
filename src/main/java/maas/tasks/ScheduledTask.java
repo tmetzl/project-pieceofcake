75f2dffe9ee2223b9ab374da3dc413f5ee3c442a
Tim Metzler <tim.metzler@inf.h-brs.de>
2018-01-03 23:20:50 +0100

    Changed ScheduledTask class to work with Date objects

--- src/main/java/maas/tasks/ScheduledTask.java
+++ src/main/java/maas/tasks/ScheduledTask.java
@@ -1,42 +1,39 @@
 package maas.tasks;
 
-public class ScheduledTask<T extends Task> implements Comparable<ScheduledTask<T>>{
-	
-	private long start;
-	private long end;
+import maas.objects.Date;
+
+public class ScheduledTask<T extends Task> implements Comparable<ScheduledTask<T>> {
+
+	private Date start;
+	private Date end;
 	private T task;
-	
-	public ScheduledTask(long start, long end, T task) {
+
+	public ScheduledTask(Date start, Date end, T task) {
 		this.start = start;
 		this.end = end;
 		this.task = task;
 	}
 
-	public long getStart() {
+	public Date getStart() {
 		return start;
 	}
 
-	public long getEnd() {
+	public Date getEnd() {
 		return end;
 	}
 
 	public T getTask() {
 		return task;
 	}
-	
+
 	@Override
 	public String toString() {
-		return String.format("(%d, %d, %s)", start, end, task);
+		return String.format("(%s, %s, %s)", start.toString(), end.toString(), task);
 	}
 
 	@Override
-	public int compareTo(ScheduledTask<T> arg0) {
-		if (getStart() < arg0.getStart()) {
-			return -1;
-		} else if (getStart() > arg0.getStart()) {
-			return 1;
-		}
-		return 0;
+	public int compareTo(ScheduledTask<T> otherTask) {
+		return start.compareTo(otherTask.start);
 	}
 
 }
