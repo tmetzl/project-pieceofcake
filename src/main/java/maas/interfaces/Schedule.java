e47d09b8ee984b888cc13deeab58d5c2a8750e6e
Tim Metzler <tim.metzler@inf.h-brs.de>
2018-01-03 23:18:46 +0100

    Changed the schedule interface to return a Date object

--- src/main/java/maas/interfaces/Schedule.java
+++ src/main/java/maas/interfaces/Schedule.java
@@ -1,11 +1,12 @@
 package maas.interfaces;
 
+import maas.objects.Date;
 import maas.tasks.ScheduledTask;
 import maas.tasks.Task;
 
 public interface Schedule<T extends Task>{
 	
-	public long getEarliestCompletionTime(T task);
+	public Date getEarliestCompletionTime(T task);
 	
 	public void insert(T task);
 	
