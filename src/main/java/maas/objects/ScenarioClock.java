b09ee1f95171574bf32128fc6f1bfb570c0b0982
Tim Metzler <tim.metzler@inf.h-brs.de>
2018-01-05 15:32:15 +0100

    Cleaned up ScenarioClock class

--- src/main/java/maas/objects/ScenarioClock.java
+++ src/main/java/maas/objects/ScenarioClock.java
@@ -30,30 +30,9 @@ public class ScenarioClock implements Serializable {
 		return System.currentTimeMillis() + timeOffset;
 	}
 
-	public Date getDate1() {
-		if (started) {
-			long millisSinceStartUp = (getSynchronizedTime() - startingTime);
-			long time1 = millisSinceStartUp * 86400l / SECONDS_PER_SCENARIO_DAY / 1000l;
-			long secondsSinceStartUp = (getSynchronizedTime() - startingTime) / 1000l;
-			long time = secondsSinceStartUp * 86400l / SECONDS_PER_SCENARIO_DAY;
-			System.out.println(time+"\t"+time1);
-			int second = (int) (time % 60);
-			time /= 60;
-			int minute = (int) (time % 60);
-			time /= 60;
-			int hour = (int) (time % 24);
-			time /= 24;
-			int day = (int) time;
-			return new Date(day, hour, minute, second);
-		} else {
-			return new Date(0, 0, 0, 0);
-		}
-	}
-	
 	public Date getDate() {
 		if (started) {
 			long millisSinceStartUp = (getSynchronizedTime() - startingTime);
-			//System.out.println(millisSinceStartUp);
 			long time = millisSinceStartUp * 86400l / (SECONDS_PER_SCENARIO_DAY * 1000l);
 			int second = (int) (time % 60);
 			time /= 60;
@@ -69,21 +48,7 @@ public class ScenarioClock implements Serializable {
 	}
 	
 	public static long millisFromScenarioSeconds(long scenarioSeconds) {
-		long millis = (scenarioSeconds * SECONDS_PER_SCENARIO_DAY * 1000l) / 86400;
-		return millis;
-	}
-	
-	public static void main(String[] args) {
-		ScenarioClock clock = new ScenarioClock();
-		clock.start();
-		try {
-			Thread.sleep(500);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		System.out.println(clock.getDate());
-		System.out.println(clock.getDate1());
+		return (scenarioSeconds * SECONDS_PER_SCENARIO_DAY * 1000l) / 86400;
 	}
 
 }
