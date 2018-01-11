26ebcd6130ebd9c0ba487771c2473c00c58f8dd2
Tim Metzler <tim.metzler@inf.h-brs.de>
2018-01-11 22:36:55 +0100

    Removed Thread.sleep() from ScenarioClockTest

--- src/test/java/org/pieceofcake/objects/ScenarioClockTest.java
+++ src/test/java/org/pieceofcake/objects/ScenarioClockTest.java
@@ -11,27 +11,28 @@ public class ScenarioClockTest {
 	public void testGetDate() throws InterruptedException {
 		ScenarioClock clock = new ScenarioClock();
 		assertEquals(new Date(0, 0, 0, 0), clock.getDate());
-		clock.setStartingTime(System.currentTimeMillis());
-		Thread.sleep(1000);
+		
+		// Set starting time to one second in the past
+		clock.setStartingTime(System.currentTimeMillis() - 1000);
 		Date dateAfterOneSecond = clock.getDate();
-		long expectedHours = 24/ScenarioClock.SECONDS_PER_SCENARIO_DAY;
+		long expectedHours = 24 / ScenarioClock.SECONDS_PER_SCENARIO_DAY;
 		assertEquals(expectedHours, dateAfterOneSecond.getHour());
 	}
-	
+
 	@Test
 	public void testMillisFromScenarioSeconds() {
-		long secondsInADay = 24*60*60;
-		long millisPerScenarioDay = ScenarioClock.SECONDS_PER_SCENARIO_DAY*1000l;
-		assertEquals(millisPerScenarioDay, ScenarioClock.millisFromScenarioSeconds(secondsInADay));		
+		long secondsInADay = 24 * 60 * 60;
+		long millisPerScenarioDay = ScenarioClock.SECONDS_PER_SCENARIO_DAY * 1000l;
+		assertEquals(millisPerScenarioDay, ScenarioClock.millisFromScenarioSeconds(secondsInADay));
 	}
-	
+
 	@Test
 	public void testTimeOffset() {
 		ScenarioClock clock = new ScenarioClock();
 		clock.setTimeOffset(1000l);
 		long measuredOffset = clock.getSynchronizedTime() - System.currentTimeMillis();
 		assertTrue(950l < measuredOffset);
-		assertTrue(1050 > measuredOffset);		
+		assertTrue(1050 > measuredOffset);
 	}
 
 }
