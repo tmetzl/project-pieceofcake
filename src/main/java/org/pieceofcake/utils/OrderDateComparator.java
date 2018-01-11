2f2207c5cadc67201e2020971b9aaea9bdc65776
sogolhaghighat <sogol.haghighat@gmail.com>
2018-01-11 19:10:43 +0100

    Refactored the OrderDateComparator and OrderDueDateComparator to use the correct compare method

--- src/main/java/org/pieceofcake/utils/OrderDateComparator.java
+++ src/main/java/org/pieceofcake/utils/OrderDateComparator.java
@@ -4,14 +4,11 @@ import java.util.Comparator;
 
 import org.pieceofcake.objects.Order;
 
-public class OrderDateComparator implements Comparator<Order>{
+public class OrderDateComparator implements Comparator<Order> {
 
 	@Override
 	public int compare(Order order1, Order order2) {
-		int orderDate1 = order1.getOrderDate();
-		int orderDate2 = order2.getOrderDate();	
-		
-		return orderDate1 - orderDate2;
+		return order1.getOrderDate().compareTo(order2.getOrderDate());
 	}
 
 }
