41a4ff83b3c7caf7964e4fac1d50bdcea7e07884
Tim Metzler <tim.metzler@inf.h-brs.de>
2018-01-16 22:12:33 +0100

    Changed the names in the JobTest

---
+++ src/test/java/org/pieceofcake/objects/JobTest.java
@@ -0,0 +1,42 @@
+package org.pieceofcake.objects;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.pieceofcake.objects.Date;
+import org.pieceofcake.tasks.KneadingTask;
+import org.pieceofcake.tasks.Task;
+
+
+
+public class JobTest {
+
+	@Test
+	public void testGetters() {
+		KneadingTask kneadingTask = new KneadingTask();
+		kneadingTask.setOrderId("order-001");
+		kneadingTask.setProductId("Bread");
+		kneadingTask.setReleaseDate(new Date(1, 2, 0, 0));
+		kneadingTask.setDueDate(new Date(1, 7, 30, 0));
+		kneadingTask.setKneadingTime(2400);
+		kneadingTask.setRestingTime(1200);
+
+		Job<KneadingTask> scheduledTask = new Job<>(new Date(1, 1, 15, 0), new Date(1, 1, 55, 0),
+				kneadingTask);
+		assertEquals(new Date(1, 1, 15, 0), scheduledTask.getStart());
+		assertEquals(new Date(1, 1, 55, 0), scheduledTask.getEnd());
+
+		Task taskFromScheduledTask = scheduledTask.getTask();
+		assertTrue(taskFromScheduledTask instanceof KneadingTask);
+
+		KneadingTask kneadingTaskFromScheduledTask = (KneadingTask) taskFromScheduledTask;
+		assertEquals(2400l, kneadingTaskFromScheduledTask.getKneadingTime());
+		assertEquals(1200l, kneadingTaskFromScheduledTask.getRestingTime());
+		assertEquals(new Date(1, 7, 30, 0), kneadingTaskFromScheduledTask.getDueDate());
+		assertEquals(new Date(1, 2, 0, 0), kneadingTaskFromScheduledTask.getReleaseDate());
+		assertEquals("order-001", kneadingTaskFromScheduledTask.getOrderId());
+		assertEquals("Bread", kneadingTaskFromScheduledTask.getProductId());
+	}
+
+}
