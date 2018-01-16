4fe8ec31d855b2a2bcb920b6b1ef55277043764e
Tim Metzler <tim.metzler@inf.h-brs.de>
2018-01-16 22:09:44 +0100

    ItemPrepSchedule now extends ProductionSchedule

--- src/main/java/org/pieceofcake/schedules/ItemPrepSchedule.java
+++ src/main/java/org/pieceofcake/schedules/ItemPrepSchedule.java
@@ -1,129 +1,38 @@
 package org.pieceofcake.schedules;
 
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
-
-import org.pieceofcake.interfaces.Schedule;
 import org.pieceofcake.objects.Date;
+import org.pieceofcake.objects.Job;
 import org.pieceofcake.tasks.ItemPrepTask;
-import org.pieceofcake.tasks.ScheduledTask;
 
-public class ItemPrepSchedule implements Schedule<ItemPrepTask> {
+public class ItemPrepSchedule extends ProductionSchedule<ItemPrepTask> {
 
-	private List<ScheduledTask<ItemPrepTask>> schedule;
-
-	public ItemPrepSchedule() {
-		this.schedule = new LinkedList<>();
+	@Override
+	public long getProductionTime(Job<ItemPrepTask> prevJob, Job<ItemPrepTask> nextJob, ItemPrepTask task) {
+		return task.getNumOfItems() * task.getItemPrepTime();
 	}
 
 	@Override
-	public Date getEarliestCompletionTime(ItemPrepTask task) {
-		long completionTimeInSeconds;
-		if (schedule.isEmpty()) {
-			completionTimeInSeconds = task.getReleaseDate().toSeconds() + task.getNumOfItems() * task.getItemPrepTime();
-			return new Date(completionTimeInSeconds);
-		}
-		Date startDate = task.getReleaseDate();
-		int remainingItems = task.getNumOfItems();
-		for (ScheduledTask<ItemPrepTask> scheduledTask : schedule) {
-			// Check how much time we have between the last task and the next
-			// task
-			long availableTime = scheduledTask.getStart().toSeconds() - startDate.toSeconds();
-			// Check how many items we could fit in that slot
-			int items = (int) (availableTime / task.getItemPrepTime());
-			// Check if all remainingItems fit
-			if (remainingItems - items <= 0) {
-				completionTimeInSeconds = startDate.toSeconds() + remainingItems * task.getItemPrepTime();
-				return new Date(completionTimeInSeconds);
-			} else if (items > 0) {
-				remainingItems = remainingItems - items;
-			}
-			if (startDate.compareTo(scheduledTask.getEnd()) < 0) {
-				startDate = scheduledTask.getEnd();
-			}
-		}
-		completionTimeInSeconds = startDate.toSeconds() + remainingItems * task.getItemPrepTime();
-		return new Date(completionTimeInSeconds);
-	}
-
-	@Override
-	public void insert(ItemPrepTask task) {
-		if (schedule.isEmpty()) {
-			Date completionDate = new Date(
-					task.getReleaseDate().toSeconds() + task.getNumOfItems() * task.getItemPrepTime());
-			schedule.add(new ScheduledTask<>(task.getReleaseDate(), completionDate, task));
-			return;
-		}
-		List<ScheduledTask<ItemPrepTask>> subtasks = new LinkedList<>();
-		Date startDate = task.getReleaseDate();
-		int remainingItems = task.getNumOfItems();
-		for (ScheduledTask<ItemPrepTask> scheduledTask : schedule) {
-			// Check how much time we have between the last task and the next
-			// task
-			long availableTime = scheduledTask.getStart().toSeconds() - startDate.toSeconds();
-			// Check how many items fit in that slot
-			int items = (int) (availableTime / task.getItemPrepTime());
-			// Check if all remainingItems fit
-			if (remainingItems - items <= 0) {
-				ItemPrepTask subtask = createSubTask(task, remainingItems);
-
-				long completionTimeInSeconds = startDate.toSeconds() + remainingItems * task.getItemPrepTime();
-				Date endDate = new Date(completionTimeInSeconds);
-				subtasks.add(new ScheduledTask<>(startDate, endDate, subtask));
-				remainingItems = 0;
-				break;
-			} else if (items > 0) {
-				// Add a new subtask
-				ItemPrepTask subtask = createSubTask(task, items);
-
-				long completionTimeInSeconds = startDate.toSeconds() + items * task.getItemPrepTime();
-				Date endDate = new Date(completionTimeInSeconds);
-				subtasks.add(new ScheduledTask<>(startDate, endDate, subtask));
-				remainingItems = remainingItems - items;
-			}
-			if (startDate.compareTo(scheduledTask.getEnd()) < 0) {
-				startDate = scheduledTask.getEnd();
-			}
-		}
-		// Check if items still remain
-		if (remainingItems > 0) {
-			ItemPrepTask subtask = createSubTask(task, remainingItems);
-
-			long completionTimeInSeconds = startDate.toSeconds() + remainingItems * task.getItemPrepTime();
-			Date endDate = new Date(completionTimeInSeconds);
-			subtasks.add(new ScheduledTask<>(startDate, endDate, subtask));
-		}
-		// Add all new tasks and sort
-		schedule.addAll(subtasks);
-		Collections.sort(schedule);
-	}
-
-	@Override
-	public ScheduledTask<ItemPrepTask> getNextScheduledTask() {
-		if (!schedule.isEmpty()) {
-			return schedule.get(0);
-		}
+	public ItemPrepTask addToJob(Job<ItemPrepTask> job, ItemPrepTask task) {
 		return null;
 	}
 
 	@Override
-	public void removeFirst() {
-		if (!schedule.isEmpty()) {
-			schedule.remove(0);
+	public ItemPrepTask addBetweenJobs(Job<ItemPrepTask> prevJob, Job<ItemPrepTask> nextJob, ItemPrepTask task) {
+		Date startDate = task.getReleaseDate();
+		if (prevJob != null && startDate.compareTo(prevJob.getEnd()) < 0) {
+			startDate = prevJob.getEnd();
 		}
-	}
-
-	private ItemPrepTask createSubTask(ItemPrepTask task, int numOfItems) {
-		ItemPrepTask subtask = new ItemPrepTask();
-		subtask.setReleaseDate(task.getReleaseDate());
-		subtask.setDueDate(task.getDueDate());
-		subtask.setOrderId(task.getOrderId());
-		subtask.setProductId(task.getProductId());
-
-		subtask.setItemPrepTime(task.getItemPrepTime());
-		subtask.setNumOfItems(numOfItems);
-		return subtask;
+		long availableTime = 12*60*60;
+		if (nextJob != null) {
+			availableTime = nextJob.getStart().toSeconds() - startDate.toSeconds();
+		}
+		int items = Math.min((int) (availableTime / task.getItemPrepTime()), task.getNumOfItems());
+		if (items > 0) {
+			ItemPrepTask subtask = task.copy();
+			subtask.setNumOfItems(items);
+			return subtask;
+		}
+		return null;
 	}
 
 }
