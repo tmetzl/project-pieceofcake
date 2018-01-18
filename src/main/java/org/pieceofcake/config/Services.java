246ee4f31aa6dd769a94876c9c6bc20fff2365a1
Tim Metzler <tim.metzler@inf.h-brs.de>
2018-01-18 22:08:15 +0100

    Added services to the Service class

--- src/main/java/org/pieceofcake/config/Services.java
+++ src/main/java/org/pieceofcake/config/Services.java
@@ -7,11 +7,19 @@ public class Services {
 	}
 	
 	public static final String STARTUP = "startUp-service";
+	public static final String STARTUP_NAME = "startUp-service-name";
+	public static final String TIME = "time-service";
+	public static final String TIME_NAME = "time-service-name";
+	public static final String ORDER = "bakery";
+	public static final String ORDER_NAME = "bakery-ordering";
+	
 	public static final String KNEAD = "kneading-service";
 	public static final String REST = "resting-service";
 	public static final String PREP = "prep-service";
 	public static final String BAKE = "baking-service";
 	public static final String BOX = "boxing-service";
 	public static final String DELIVERY = "delivery-service";
+	
+	
 
 }
