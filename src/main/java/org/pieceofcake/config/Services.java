a6c3f800ae806472fbe13dff1e7622909e566311
sogolhaghighat <sogol.haghighat@gmail.com>
2018-01-18 17:46:01 +0100

    Created the Services class to contain agents services as strings

---
+++ src/main/java/org/pieceofcake/config/Services.java
@@ -0,0 +1,17 @@
+package org.pieceofcake.config;
+
+public class Services {
+	
+	private Services() {
+		
+	}
+	
+	public static final String STARTUP = "startUp-service";
+	public static final String KNEAD = "kneading-service";
+	public static final String REST = "resting-service";
+	public static final String PREP = "prep-service";
+	public static final String BAKE = "baking-service";
+	public static final String BOX = "boxing-service";
+	public static final String DELIVERY = "delivery-service";
+
+}
