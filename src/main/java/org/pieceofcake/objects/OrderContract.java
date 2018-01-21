8af56de058dc0dc79d469f8499482f39c77eeeba
sogolhaghighat <sogol.haghighat@gmail.com>
2018-01-21 23:26:22 +0100

    Created the OrderContract class to store scheduled orders

---
+++ src/main/java/org/pieceofcake/objects/OrderContract.java
@@ -0,0 +1,139 @@
+package org.pieceofcake.objects;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import org.pieceofcake.tasks.*;
+
+import jade.core.AID;
+
+public class OrderContract implements Serializable {
+
+	private static final long serialVersionUID = 5764464650645828722L;
+
+	private Order order;
+	private Map<AID, List<KneadingTask>> kneadingTaskMap;
+	private Map<AID, List<RestingTask>> restingTaskMap;
+	private Map<AID, List<ItemPrepTask>> itemPrepTaskMap;
+	private Map<AID, List<BakingTask>> bakingTaskMap;
+	private Map<AID, List<CoolingTask>> coolingTaskMap;
+	private Map<AID, List<DeliveryTask>> deliveryTaskMap;
+
+	public OrderContract(Order order) {
+		this.order = order;
+		this.kneadingTaskMap = new HashMap<>();
+		this.restingTaskMap = new HashMap<>();
+		this.itemPrepTaskMap = new HashMap<>();
+		this.bakingTaskMap = new HashMap<>();
+		this.coolingTaskMap = new HashMap<>();
+		this.deliveryTaskMap = new HashMap<>();
+	}
+
+	public Order getOrder() {
+		return order;
+	}
+
+	private <T extends Task> void addTask(AID agentId, T task, Map<AID, List<T>> taskMap) {
+		List<T> existingTasks = taskMap.get(agentId);
+		if (existingTasks == null) {
+			existingTasks = new LinkedList<>();
+		}
+		existingTasks.add(task);
+		taskMap.put(agentId, existingTasks);
+	}
+
+	private <T extends Task> List<T> getTasks(Map<AID, List<T>> taskMap) {
+		List<T> tasks = new LinkedList<>();
+		for (List<T> existingTasks : taskMap.values()) {
+			tasks.addAll(existingTasks);
+		}
+		return tasks;
+	}
+
+	public List<AID> getAgents() {
+		List<AID> agents = new LinkedList<>();
+		agents.addAll(kneadingTaskMap.keySet());
+		agents.addAll(restingTaskMap.keySet());
+		agents.addAll(itemPrepTaskMap.keySet());
+		agents.addAll(bakingTaskMap.keySet());
+		agents.addAll(coolingTaskMap.keySet());
+		agents.addAll(deliveryTaskMap.keySet());
+		return agents;
+	}
+
+	public Map<AID, List<KneadingTask>> getKneadingTaskMap() {
+		return kneadingTaskMap;
+	}
+
+	public List<KneadingTask> getKneadingTasks() {
+		return getTasks(kneadingTaskMap);
+	}
+
+	public void addKneadingTask(AID agentId, KneadingTask task) {
+		addTask(agentId, task, kneadingTaskMap);
+	}
+
+	public Map<AID, List<RestingTask>> getRestingTaskMap() {
+		return restingTaskMap;
+	}
+
+	public List<RestingTask> getRestingTasks() {
+		return getTasks(restingTaskMap);
+	}
+
+	public void addRestingTask(AID agentId, RestingTask task) {
+		addTask(agentId, task, restingTaskMap);
+	}
+
+	public Map<AID, List<ItemPrepTask>> getItemPrepTaskMap() {
+		return itemPrepTaskMap;
+	}
+
+	public List<ItemPrepTask> getItemPrepTasks() {
+		return getTasks(itemPrepTaskMap);
+	}
+
+	public void addItemPrepTask(AID agentId, ItemPrepTask task) {
+		addTask(agentId, task, itemPrepTaskMap);
+	}
+
+	public Map<AID, List<BakingTask>> getBakingTaskMap() {
+		return bakingTaskMap;
+	}
+
+	public List<BakingTask> getBakingTasks() {
+		return getTasks(bakingTaskMap);
+	}
+
+	public void addBakingTask(AID agentId, BakingTask task) {
+		addTask(agentId, task, bakingTaskMap);
+	}
+
+	public Map<AID, List<CoolingTask>> getCoolingTaskMap() {
+		return coolingTaskMap;
+	}
+
+	public List<CoolingTask> getCoolingTasks() {
+		return getTasks(coolingTaskMap);
+	}
+
+	public void addCoolingTask(AID agentId, CoolingTask task) {
+		addTask(agentId, task, coolingTaskMap);
+	}
+
+	public Map<AID, List<DeliveryTask>> getDeliveryTaskMap() {
+		return deliveryTaskMap;
+	}
+
+	public List<DeliveryTask> getDeliveryTasks() {
+		return getTasks(deliveryTaskMap);
+	}
+
+	public void addDeliveryTask(AID agentId, DeliveryTask task) {
+		addTask(agentId, task, deliveryTaskMap);
+	}
+
+}
