8c3f1abfec0a534c18fcde175005aa155171b741
sogolhaghighat <sogol.haghighat@gmail.com>
2018-01-21 14:43:40 +0100

    Removed the private behaviour class AdvertiseTask from the OrderAgent class

--- src/main/java/org/pieceofcake/agents/OrderAgent.java
+++ src/main/java/org/pieceofcake/agents/OrderAgent.java
@@ -1,30 +1,14 @@
 package org.pieceofcake.agents;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-
 import org.json.JSONObject;
-import org.pieceofcake.behaviours.FindAgents;
 import org.pieceofcake.behaviours.ReceiveStartingTime;
 import org.pieceofcake.behaviours.SynchronizeClock;
 import org.pieceofcake.config.Protocols;
 import org.pieceofcake.config.Services;
 import org.pieceofcake.objects.Bakery;
-import org.pieceofcake.objects.Bid;
-import org.pieceofcake.objects.Date;
 import org.pieceofcake.objects.Order;
-import org.pieceofcake.objects.Proposal;
-import org.pieceofcake.tasks.Task;
-import org.pieceofcake.utils.BidCompletionTimeComparator;
-import org.pieceofcake.utils.OrderDueDateComparator;
 
-import jade.core.AID;
-import jade.core.behaviours.Behaviour;
 import jade.core.behaviours.CyclicBehaviour;
-import jade.core.behaviours.OneShotBehaviour;
 import jade.core.behaviours.SequentialBehaviour;
 import jade.domain.DFService;
 import jade.domain.FIPAException;
@@ -126,120 +110,5 @@ public class OrderAgent extends SynchronizedAgent {
 			}
 		}
 
-	}
-
-	private class AdvertiseTasks extends SequentialBehaviour {
-
-		private List<AID> agentList;
-		private String protocol;
-		private String serviceType;
-		private String serviceName;
-		private Task task;
-		private List<Bid> bestTaskOffered;
-		private int numOfReplies = 0;
-		private Map<AID, Proposal> proposals;
-
-		public AdvertiseTasks(Task task, String serviceType, String serviceName, List<Bid> bestTaskOffered,
-				String protocol) {
-			this.task = task;
-			this.protocol = protocol;
-			this.serviceType = serviceType;
-			this.serviceName = serviceName;
-			this.bestTaskOffered = bestTaskOffered;
-			agentList = new LinkedList<AID>();
-			proposals = new HashMap<>();
-			// bestTaskOffered = new HashMap<>();
-
-			this.addSubBehaviour(new FindAgents(serviceType, serviceName, agentList));
-			this.addSubBehaviour(new SendTasks());
-			this.addSubBehaviour(new ReceiveOffers(proposals));
-			this.addSubBehaviour(new ProcessProposals());
-
-		}
-
-		private class SendTasks extends OneShotBehaviour {
-
-			@Override
-			public void action() {
-				// Date currentDate = getScenarioClock().getDate();
-				// String output = String.format("%nDay %d %02d:%02d:%02d%n%s",
-				// currentDate.getDay(),
-				// currentDate.getHour(), currentDate.getMinute(),
-				// currentDate.getSecond(), task);
-				// logger.log(Logger.INFO, output);
-
-				ACLMessage msg = new ACLMessage(ACLMessage.CFP);
-				// Add all found agents for this task
-				for (int i = 0; i < agentList.size(); i++) {
-					msg.addReceiver(agentList.get(i));
-				}
-				msg.setLanguage("English");
-				msg.setOntology("Bakery-order-ontology");
-				msg.setProtocol(protocol);
-				String content = task.toJSONObject().toString();
-				msg.setContent(content);
-				myAgent.send(msg);
-			}
-		}
-
-		private class ReceiveOffers extends Behaviour {
-
-			private boolean allOffersReceived = false;
-
-			public ReceiveOffers(Map<AID, Proposal> proposals) {
-
-			}
-
-			@Override
-			public void action() {
-				MessageTemplate msgTemplate = MessageTemplate.MatchProtocol(protocol);
-				ACLMessage offer = myAgent.receive(msgTemplate);
-				if (offer != null) {
-
-					JSONObject jsonProposal = new JSONObject(offer.getContent());
-					Proposal proposal = new Proposal();
-					proposal.fromJSONObject(jsonProposal);
-					proposals.put(offer.getSender(), proposal);
-					numOfReplies++;
-					if (numOfReplies >= agentList.size()) {
-						allOffersReceived = true;
-					}
-				} else {
-					block();
-				}
-			}
-
-			@Override
-			public boolean done() {
-				return allOffersReceived;
-			}
-
-		}
-
-		private class ProcessProposals extends OneShotBehaviour {
-
-			private int numOfItems;
-			private List<Bid> bids;
-
-			public ProcessProposals() {
-				numOfItems = task.getNumOfItems();
-			}
-
-			@Override
-			public void action() {
-				for (AID agentId : agentList) {
-					for (int i = 0; i < numOfItems; i++) {
-						Bid bid = new Bid();
-						bid.setAgentId(agentId);
-						bid.setCompletionTime(proposals.get(agentId).getCompletionTimes().get(i));
-						bids.add(bid);
-					}
-				}
-				Collections.sort(bids, new BidCompletionTimeComparator());
-				for (int i = 0; i < numOfItems; i++) {
-					bestTaskOffered.add(bids.get(i));
-				}
-			}
-		}
 	}
 }
