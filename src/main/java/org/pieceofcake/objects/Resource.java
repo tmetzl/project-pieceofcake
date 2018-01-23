469ac173eaa6ab4c649d4ec06a9545c0c2bc975e
Tim Metzler <tim.metzler@inf.h-brs.de>
2018-01-23 15:03:16 +0100

    Created an object for intermediate resources

---
+++ src/main/java/org/pieceofcake/objects/Resource.java
@@ -0,0 +1,49 @@
+package org.pieceofcake.objects;
+
+import org.json.JSONObject;
+
+public class Resource {
+	
+	private String resourceType;
+	private String productId;
+	private int amount;
+	
+	public String getResourceType() {
+		return resourceType;
+	}
+	
+	public void setResourceType(String resourceType) {
+		this.resourceType = resourceType;
+	}
+	
+	public String getProductId() {
+		return productId;
+	}
+	
+	public void setProductId(String productId) {
+		this.productId = productId;
+	}
+	
+	public int getAmount() {
+		return amount;
+	}
+	
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+	
+	public JSONObject toJSONObject() {
+		JSONObject jsonRequest = new JSONObject();
+		jsonRequest.put("resource_type", resourceType);
+		jsonRequest.put("product_id", productId);
+		jsonRequest.put("amount", amount);		
+		return jsonRequest;
+	}
+	
+	public void fromJSONObject(JSONObject jsonRequest) {
+		resourceType = jsonRequest.getString("resource_type");
+		productId = jsonRequest.getString("product_id");
+		amount = jsonRequest.getInt("amount");
+	}
+
+}
