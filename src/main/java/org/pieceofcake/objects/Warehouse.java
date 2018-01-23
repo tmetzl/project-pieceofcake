af28a75fbc918f76c253b1cde46a749d6cee1097
Tim Metzler <tim.metzler@inf.h-brs.de>
2018-01-23 15:03:32 +0100

    Created an object to store intermediate resources

---
+++ src/main/java/org/pieceofcake/objects/Warehouse.java
@@ -0,0 +1,48 @@
+package org.pieceofcake.objects;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.NoSuchElementException;
+
+public class Warehouse {
+	
+	private Map<String, Integer> resourceStorage;
+	
+	public Warehouse() {
+		this.resourceStorage = new HashMap<>();
+	}
+	
+	public void addResource(Resource resource) {
+		String resourceKey = resource.getResourceType() + resource.getProductId();
+		Integer amount = resourceStorage.get(resourceKey);
+		if (amount == null) {
+			amount = 0;
+		}
+		amount += resource.getAmount();
+		resourceStorage.put(resourceKey, amount);
+	}
+	
+	public boolean hasResource(Resource resource) {
+		String resourceKey = resource.getResourceType() + resource.getProductId();
+		Integer amount = resourceStorage.get(resourceKey);
+		if (amount == null) {
+			amount = 0;
+		}
+		return amount == resource.getAmount();
+	}
+	
+	public void takeResource(Resource resource) {
+		String resourceKey = resource.getResourceType() + resource.getProductId();
+		Integer amount = resourceStorage.get(resourceKey);
+		if (amount != null && amount >= resource.getAmount()) {
+			resourceStorage.put(resourceKey, amount - resource.getAmount());
+		} else {
+			throw new NoSuchElementException();
+		}
+	}
+	
+	public void clear() {
+		resourceStorage.clear();
+	}
+
+}
