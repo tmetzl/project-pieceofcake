3987129a5fbd90125eae6319286aee3e494e121e
sogolhaghighat <sogol.haghighat@gmail.com>
2018-01-23 01:26:06 +0100

    Created the TaskDescriptor interface

---
+++ src/main/java/org/pieceofcake/interfaces/TaskDescriptor.java
@@ -0,0 +1,25 @@
+package org.pieceofcake.interfaces;
+
+import java.util.List;
+
+import org.pieceofcake.objects.Date;
+import org.pieceofcake.objects.OrderContract;
+import org.pieceofcake.tasks.Task;
+
+import jade.core.AID;
+
+public interface TaskDescriptor<T extends Task> {
+
+	public List<T> prepareTasks();
+
+	public Date getDueDate();
+
+	public void addTaskToOrder(AID agentId, T task, OrderContract contract);
+
+	public String getServiceName();
+
+	public String getBakeryName();
+
+	public String getProtocol();
+
+}
