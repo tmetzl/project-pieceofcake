6b2fee351edda0b09d767bfab775311df883a6e4
Tim Metzler <tim.metzler@inf.h-brs.de>
2018-01-23 15:35:48 +0100

    Added a behaviour to handle resource requests

---
+++ src/main/java/org/pieceofcake/behaviours/ResourceHandler.java
@@ -0,0 +1,126 @@
+package org.pieceofcake.behaviours;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+
+import org.json.JSONObject;
+import org.pieceofcake.config.Protocols;
+import org.pieceofcake.config.Resources;
+import org.pieceofcake.objects.Resource;
+import org.pieceofcake.objects.Warehouse;
+
+import jade.core.AID;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.core.behaviours.OneShotBehaviour;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+
+public class ResourceHandler extends CyclicBehaviour {
+
+	private static final long serialVersionUID = -6118994291781057665L;
+	
+	private Warehouse warehouse;
+	private Map<String, Queue<ResourceRequest>> resourceRequests;
+	
+	public ResourceHandler(Warehouse warehouse) {
+		this.warehouse = warehouse;
+		this.resourceRequests = new HashMap<>();
+	}
+
+	@Override
+	public void action() {
+		MessageTemplate msgTemplate = MessageTemplate.MatchProtocol(Protocols.RESOURCE);
+		ACLMessage msg = myAgent.receive(msgTemplate);
+		if (msg != null) {
+			Resource resource = new Resource();
+			resource.fromJSONObject(new JSONObject(msg.getContent()));
+			if (msg.getPerformative() == ACLMessage.REQUEST) {
+				ResourceRequest request = new ResourceRequest(msg.getSender(), msg.getConversationId(), resource);
+				addToRequestQueue(request);
+			} else if (msg.getPerformative() == ACLMessage.INFORM) {
+				warehouse.addResource(resource);
+			}
+		} else {
+			block();
+		}
+		
+	}
+	
+	public void addToRequestQueue(ResourceRequest request) {
+		String resourceKey = request.getResource().getResourceType() + request.getResource().getProductId();
+		Queue<ResourceRequest> requestQueue = resourceRequests.get(resourceKey);
+		if (requestQueue == null) {
+			requestQueue = new LinkedList<>();
+		}
+		requestQueue.add(request);
+		resourceRequests.put(resourceKey, requestQueue);
+		notifyRequesters(request.getResource());
+	}
+	
+	public void notifyRequesters(Resource resource) {
+		String resourceKey = resource.getResourceType() + resource.getProductId();
+		Queue<ResourceRequest> resourceQueue = resourceRequests.get(resourceKey);
+		if (resourceQueue == null) {
+			resourceQueue = new LinkedList<>();
+			resourceRequests.put(resourceKey, resourceQueue);
+		}
+		ResourceRequest request = resourceQueue.peek();
+		while (request != null && warehouse.hasResource(request.getResource())) {
+			if (!resource.getResourceType().equals(Resources.FRESH_DOUGH) && !resource.getResourceType().equals(Resources.RESTED_DOUGH)) {
+				warehouse.takeResource(request.getResource());
+			}
+			myAgent.addBehaviour(new InformRequester(resourceQueue.poll()));
+			request = resourceQueue.peek();
+		}
+	}
+	
+	private class InformRequester extends OneShotBehaviour {
+
+		private static final long serialVersionUID = 8044766592392978206L;
+		
+		private ResourceRequest request;
+		
+		public InformRequester(ResourceRequest request) {
+			this.request = request;
+		}
+
+		@Override
+		public void action() {
+			ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
+			msg.setProtocol(Protocols.RESOURCE);
+			msg.setConversationId(request.getRequestId());
+			msg.addReceiver(request.getRequester());
+			msg.setContent(request.getResource().toJSONObject().toString());
+			myAgent.send(msg);			
+		}
+		
+	}	
+	
+	private class ResourceRequest {
+		
+		private AID requester;
+		private String requestId;
+		private Resource resource;
+		
+		public ResourceRequest(AID requester, String requestId, Resource resource) {
+			this.requester = requester;
+			this.resource = resource;
+		}
+		
+		public AID getRequester() {
+			return requester;
+		}
+		
+		public String getRequestId() {
+			return requestId;
+		}
+		public Resource getResource() {
+			return resource;
+		}
+		
+		
+	}
+
+}
