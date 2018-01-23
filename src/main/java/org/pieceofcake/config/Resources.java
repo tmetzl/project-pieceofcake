38208ad0fcb6d8c2b7d14c7c27b965c13c6fa3b7
Tim Metzler <tim.metzler@inf.h-brs.de>
2018-01-23 15:35:03 +0100

    Added a class with constants for resource types

---
+++ src/main/java/org/pieceofcake/config/Resources.java
@@ -0,0 +1,8 @@
+package org.pieceofcake.config;
+
+public class Resources {
+
+	public static final Object FRESH_DOUGH = "fresh-dough";
+	public static final Object RESTED_DOUGH = "rested-dough";
+
+}
