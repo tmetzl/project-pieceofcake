7482632a1bfd069f93053fea9ccbf194fd640fd3
Tim Metzler <tim.metzler@inf.h-brs.de>
2018-01-24 01:49:45 +0100

    Added the Machine interface that describes how tasks are handled

---
+++ src/main/java/org/pieceofcake/interfaces/Machine.java
@@ -0,0 +1,28 @@
+package org.pieceofcake.interfaces;
+
+import java.io.Serializable;
+
+import org.pieceofcake.objects.Job;
+import org.pieceofcake.tasks.Task;
+
+import jade.core.behaviours.Behaviour;
+
+public interface Machine<T extends Task> extends Serializable {
+	
+	public Schedule<T> getScheduleOfDay(int day);
+
+	public T getTask(String msg);
+
+	public Behaviour getJobProcessor(Job<T> job);
+	
+	public void aquireMachine();
+	
+	public void releaseMachine();
+	
+	public String getServiceType();
+	
+	public String getBakeryName();
+	
+	public String getProtocol();
+
+}
