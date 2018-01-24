724f6cde92a65c8976f12d7a7a68fc08846d9036
Tim Metzler <tim.metzler@inf.h-brs.de>
2018-01-24 17:07:52 +0100

    Added resource to the Resources class

--- src/main/java/org/pieceofcake/config/Resources.java
+++ src/main/java/org/pieceofcake/config/Resources.java
@@ -8,5 +8,8 @@ public class Resources {
 
 	public static final String FRESH_DOUGH = "fresh-dough";
 	public static final String RESTED_DOUGH = "rested-dough";
+	public static final String PREPPED_ITEM = "prepped-item";
+	public static final String BAKED_ITEM = "baked-item";
+	public static final String COOLED_ITEM = "cooled-item";
 
 }
