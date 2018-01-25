701e53a33ab5381788cf37dd1106217178c97671
Tim Metzler <tim.metzler@inf.h-brs.de>
2018-01-25 23:32:51 +0100

    Changed SingleMachine to use the SequentialJobExecutor

--- src/main/java/org/pieceofcake/machines/SingleMachine.java
+++ src/main/java/org/pieceofcake/machines/SingleMachine.java
@@ -1,40 +1,23 @@
 package org.pieceofcake.machines;
 
-import java.util.concurrent.Semaphore;
-
+import org.pieceofcake.behaviours.SequentialJobExecutor;
+import org.pieceofcake.interfaces.JobExecutor;
 import org.pieceofcake.tasks.Task;
-
-import jade.util.Logger;
 
 public abstract class SingleMachine<T extends Task> extends AbstractMachine<T> {
 
 	private static final long serialVersionUID = -5488123242981992996L;
 	
-	private Semaphore semaphore;
-	
+	private SequentialJobExecutor handler;
+
 	public SingleMachine(String bakeryName, String serviceType, String protocol) {
 		super(bakeryName, serviceType, protocol);
-		this.semaphore = new Semaphore(1, true);
+		this.handler = new SequentialJobExecutor();
 	}
 
 	@Override
-	public void aquireMachine() {
-		try {
-			semaphore.acquire();
-		} catch (InterruptedException e) {
-			Logger.getJADELogger(this.getClass().getName()).log(Logger.WARNING, e.getMessage(), e);
-			Thread.currentThread().interrupt();
-		}
-	}
-	
-	@Override
-	public boolean tryAquireMachine() {
-		return semaphore.tryAcquire();
-	}
-
-	@Override
-	public void releaseMachine() {
-		semaphore.release();
+	public JobExecutor getJobHandler() {
+		return handler;
 	}
 
 }
