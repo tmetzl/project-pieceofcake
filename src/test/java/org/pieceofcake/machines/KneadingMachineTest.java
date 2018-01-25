fe12a49e8accd38f645596ad0d78d6d5f921bcc6
Tim Metzler <tim.metzler@inf.h-brs.de>
2018-01-25 01:10:57 +0100

    Expanded the KneadingMachineTest

--- src/test/java/org/pieceofcake/machines/KneadingMachineTest.java
+++ src/test/java/org/pieceofcake/machines/KneadingMachineTest.java
@@ -1,8 +1,10 @@
 package org.pieceofcake.machines;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.pieceofcake.config.Protocols;
 import org.pieceofcake.config.Services;
@@ -10,7 +12,22 @@ import org.pieceofcake.interfaces.Schedule;
 import org.pieceofcake.objects.Date;
 import org.pieceofcake.tasks.KneadingTask;
 
+import jade.core.behaviours.Behaviour;
+
 public class KneadingMachineTest {
+	
+	private KneadingTask task;
+	
+	@Before
+	public void prepareTask() {
+		task = new KneadingTask();
+		task.setOrderId("order-001");
+		task.setProductId("Bread");
+		task.setReleaseDate(new Date(4, 0, 0, 0));
+		task.setDueDate(new Date(4, 7, 30, 0));
+		task.setKneadingTime(1000);
+		task.setNumOfItems(1);
+	}
 	
 	@Test
 	public void testGetters() {
@@ -24,13 +41,6 @@ public class KneadingMachineTest {
 	@Test
 	public void testGetTask() {
 		KneadingMachine machine = new KneadingMachine("Test-Bakery");
-		
-		KneadingTask task = new KneadingTask();
-		task.setOrderId("order-001");
-		task.setProductId("Bread");
-		task.setReleaseDate(new Date(1, 2, 0, 0));
-		task.setDueDate(new Date(1, 7, 30, 0));
-		task.setNumOfItems(1);
 		
 		String msg = task.toJSONObject().toString();
 		
@@ -47,14 +57,6 @@ public class KneadingMachineTest {
 	public void testGetScheduleOfDay() {
 		KneadingMachine machine = new KneadingMachine("Test-Bakery");
 		
-		KneadingTask task = new KneadingTask();
-		task.setOrderId("order-001");
-		task.setProductId("Bread");
-		task.setReleaseDate(new Date(1, 2, 0, 0));
-		task.setDueDate(new Date(4, 7, 30, 0));
-		task.setKneadingTime(1000);
-		task.setNumOfItems(1);
-		
 		Schedule<KneadingTask> schedule = machine.getScheduleOfDay(4);
 		
 		assertNull(schedule.getNextScheduledJob());
@@ -69,6 +71,17 @@ public class KneadingMachineTest {
 		assertEquals(task.getDueDate(), taskFromSchedule.getDueDate());
 		assertEquals(task.getNumOfItems(), taskFromSchedule.getNumOfItems());
 		
+	}
+	
+	@Test
+	public void testGetJobProcessor() {
+		KneadingMachine machine = new KneadingMachine("Test-Bakery");
+		
+		Schedule<KneadingTask> schedule = machine.getScheduleOfDay(4);	
+		schedule.insert(task);
+		
+		Behaviour behaviour = machine.getJobProcessor(schedule.getNextScheduledJob());
+		assertNotNull(behaviour);
 	}
 
 }
