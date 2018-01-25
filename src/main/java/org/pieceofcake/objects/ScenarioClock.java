7ffec6202b7c31e8e1ae708c126804e8a15f9559
Tim Metzler <tim.metzler@inf.h-brs.de>
2018-01-25 23:39:22 +0100

    Changed value of scenario seconds per day in ScenarioClock

--- src/main/java/org/pieceofcake/objects/ScenarioClock.java
+++ src/main/java/org/pieceofcake/objects/ScenarioClock.java
@@ -5,7 +5,7 @@ import java.io.Serializable;
 public class ScenarioClock implements Serializable {
 
 	private static final long serialVersionUID = 4923509163246771424L;
-	public static final long SECONDS_PER_SCENARIO_DAY = 6;
+	public static final long SECONDS_PER_SCENARIO_DAY = 12;
 
 	private long startingTime;
 	private long timeOffset;
@@ -13,19 +13,19 @@ public class ScenarioClock implements Serializable {
 	public ScenarioClock() {
 		this.startingTime = Long.MAX_VALUE;
 	}
-	
+
 	public void setStartingTime(long startingTime) {
-		this.startingTime = startingTime;		
+		this.startingTime = startingTime;
 	}
-	
+
 	public void setTimeOffset(long timeOffset) {
 		this.timeOffset += timeOffset;
 	}
-	
+
 	public long getSynchronizedTime() {
 		return System.currentTimeMillis() + timeOffset;
 	}
-	
+
 	public Date getDate() {
 		long millisSinceStartUp = Math.max(0, getSynchronizedTime() - startingTime);
 		long time = millisSinceStartUp * 86400l / (SECONDS_PER_SCENARIO_DAY * 1000l);
@@ -38,7 +38,7 @@ public class ScenarioClock implements Serializable {
 		int day = (int) time;
 		return new Date(day, hour, minute, second);
 	}
-	
+
 	public static long millisFromScenarioSeconds(long scenarioSeconds) {
 		return (scenarioSeconds * SECONDS_PER_SCENARIO_DAY * 1000l) / 86400;
 	}
