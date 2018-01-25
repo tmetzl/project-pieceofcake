c034f8bb8e7daaa2a0b838e40fa32ef9a5fdf443
Tim Metzler <tim.metzler@inf.h-brs.de>
2018-01-25 23:36:45 +0100

    Fixed bug in ItemPrepMachine

--- src/main/java/org/pieceofcake/machines/ItemPrepMachine.java
+++ src/main/java/org/pieceofcake/machines/ItemPrepMachine.java
@@ -12,6 +12,7 @@ import org.pieceofcake.config.Resources;
 import org.pieceofcake.config.Services;
 import org.pieceofcake.interfaces.Schedule;
 import org.pieceofcake.objects.Job;
+import org.pieceofcake.objects.Resource;
 import org.pieceofcake.schedules.ItemPrepSchedule;
 import org.pieceofcake.tasks.ItemPrepTask;
 
@@ -45,7 +46,9 @@ public class ItemPrepMachine extends SingleMachine<ItemPrepTask> {
 	public Behaviour getJobProcessor(Job<ItemPrepTask> job) {
 		long seconds = job.getEnd().toSeconds() - job.getStart().toSeconds();		
 		SequentialBehaviour seq = new SequentialBehaviour();
-		seq.addSubBehaviour(new WaitForResources(getResource(Resources.RESTED_DOUGH, job), getBakeryName()));	
+		Resource neededResource = getResource(Resources.RESTED_DOUGH, job);
+		neededResource.setAmount(1);
+		seq.addSubBehaviour(new WaitForResources(neededResource, getBakeryName()));	
 		seq.addSubBehaviour(new WaitForDuration(seconds));
 		seq.addSubBehaviour(new UpdateResources(getResource(Resources.PREPPED_ITEM, job), getBakeryName()));
 		return seq;
