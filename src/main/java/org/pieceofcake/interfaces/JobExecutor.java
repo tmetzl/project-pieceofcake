02881be1f3b7050cec90a7f7e2e66b3d801dbdf8
Tim Metzler <tim.metzler@inf.h-brs.de>
2018-01-26 00:00:31 +0100

    Made JobExecutor extends Serializable

--- src/main/java/org/pieceofcake/interfaces/JobExecutor.java
+++ src/main/java/org/pieceofcake/interfaces/JobExecutor.java
@@ -1,9 +1,11 @@
 package org.pieceofcake.interfaces;
 
+import java.io.Serializable;
+
 import jade.core.behaviours.Behaviour;
 import jade.core.behaviours.CompositeBehaviour;
 
-public interface JobExecutor {
+public interface JobExecutor extends Serializable {
 	
 	public void addSubBehaviour(Behaviour behaviour);
 	
